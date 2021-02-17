6262492da582a37a17a68ce27e418295ccc58ce9
kidfang <50686062+kidfang@users.noreply.github.com>
2021-02-17 10:25:59 +0800

    Add files via upload

---
+++ xilinx_FPGA_dump.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#!/bin/sh
+
+P_path=$('pwd')
+num=$(xbutil list | grep xilinx | wc -l)
+
+mkdir $P_path/Basic_info >/dev/null 2>&1
+
+path=$P_path/Basic_info 
+
+mkdir -p $path/FPGA_dump_all >/dev/null 2>&1
+mkdir -p $path/FPGA_query_all >/dev/null 2>&1
+
+for (( i=0; i<$num; i=i+1 ));
+do	
+
+	echo -e "\n----------- Start Saving the FPGA"$i" status log  -----------\n"
+	xbutil dump -d $i > $path/FPGA_dump_all/FPGA"$i"_dump_log.txt
+	xbutil query -d $i > $path/FPGA_query_all/FPGA"$i"_query_log.txt
+	
+	echo -e "\n----------- FPGA"$i" ECC check  -----------\n" >> $path/FPGA_query_all/ECC_check_summary.txt
+	xbutil query -d $i | grep -A 38 "Firewall Last Error Status" >> $path/FPGA_query_all/ECC_check_summary.txt
+	echo -e "\n-------------------------------------------\n" >> $path/FPGA_query_all/ECC_check_summary.txt
+done
+
+cat $path/FPGA_dump_all/FPGA* | grep -i sc_version > $path/FPGA_dump_all/FPGA_sc_version.txt
