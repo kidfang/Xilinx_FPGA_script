a5eea56dc61481a1fc0462e7c0cb375b47e1bcb1
kidfang <50686062+kidfang@users.noreply.github.com>
2021-02-17 10:24:07 +0800

    Add files via upload

---
+++ basic_info.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#!/bin/sh
+
+result_output=$('pwd')
+
+mkdir $result_output/Basic_info
+
+lshw -c memory -short | tee  $result_output/Basic_info/mem_info.txt
+dmidecode -t memory | tee $result_output/Basic_info/mem.txt
+dmidecode -t bios | tee $result_output/Basic_info/bios.txt
+lspci | grep -i Xilinx | tee $result_output/Basic_info/xilinx_fpga_pcie.txt
+lspci -tv | tee $result_output/Basic_info/lspci_tv.txt
+lspci -vd 10ee: | tee $result_output/Basic_info/lspci_10ee.txt
+lspci | tee $result_output/Basic_info/lspci.txt
+lscpu | tee $result_output/Basic_info/lscpu.txt
+
+$result_output/speed_numa_check_all.sh 8 |  tee $result_output/Basic_info/speed_numa.txt
+$result_output/xilinx_FPGA_dump.sh
+
+#xbutil validate | tee $result_output/Basic_info/xbutil_validate.txt
