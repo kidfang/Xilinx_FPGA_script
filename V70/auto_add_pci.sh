03150e53188ff389ef4b4eb32096a87e1d031b55
kidfang <50686062+kidfang@users.noreply.github.com>
2023-08-12 17:11:10 +0800

    Update auto_add_pci.sh

--- V70/auto_add_pci.sh
+++ V70/auto_add_pci.sh
@@ -1,15 +1,19 @@
-##xilinx_addresses=($(lspci | grep "Xilinx Corporation" | awk '{print $1}'))
+#!/bin/bash
+
 xlbin_name=xilinx_v70_gen5x8_qdma_base_2
-xilinx_addresses=($(xbutil examine | grep -i "$xlbin_name" | awk '{print $1}' | sed 's/\[//g; s/\]//g'))
 json_file="global_config.json"
 
-# 將Xilinx PCIe地址合併成JSON格式的陣列字串
+#######
+##xilinx_addresses=($(lspci | grep "Xilinx Corporation" | awk '{print $1}'))
+xilinx_addresses=($(xbutil examine | grep -i "$xlbin_name" | awk '{print $1}' | sed 's/\[//g; s/\]//g'))
+
+# Conbin the Xilinx PCIe address to JSON form array
 addresses_json=$(printf '"%s",' "${xilinx_addresses[@]}")
 addresses_json="[${addresses_json%,}]"
 
-# 使用jq工具來更新JSON檔案
+# use jq to update JSON file
 jq --argjson addresses "$addresses_json" '.global_config.cards = $addresses' "$json_file" > tmp.json
-
 mv tmp.json "$json_file"
 
+# Print out the json file
 cat "$json_file" | jq
